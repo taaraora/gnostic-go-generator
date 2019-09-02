2d1f47f46c129f55e554ef671fc08dbac40ebab4
Olexandr Nazin <Olexandr.Nazin@gmail.com>
2019-09-02 21:07:38 +0300

    actions are taken out in a separate function, tests are added, minor corrections

--- render_event_consts.go
+++ render_event_consts.go
@@ -1,26 +1,41 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 func (renderer *Renderer) Render_event_consts() ([]byte, error) {
 	f := NewLineWriter()
 	f.WriteLine(`// GENERATED FILE: DO NOT EDIT!`)
 	f.WriteLine(``)
 	f.WriteLine(`package ` + renderer.Package)
-
-	for _, v := range renderer.Model.ComponentsSpecificationExtension {
-		if v.Value.Yaml != "" {
-
-			constants := strings.Split(v.Value.Yaml, "\n")
-			f.WriteLine(`const (`)
-			for i := 0; i < (len(constants) - 1); i++ {
-
-				f.WriteLine(constants[i][2:] + `FactStreamType` + `=` + `"` + constants[i][2:] + `_facts"`)
-
-			}
-			f.WriteLine(`)`)
-		}
-
+	var constants []string
+	for _, namedAny := range renderer.Model.ComponentsSpecificationExtension {
+		constants = append(constants, namedAny.Value.Yaml)
 	}
+	f.WriteLine(`const (`)
+	//generate constants FactStreamType
+	if len(constants) > 0 {
+		for _, constant := range splitLineConstants(constants[0]) {
+			f.WriteLine(constant + `FactStreamType` + `=` + `"` + constant + `_facts"`)
+		}
+	}
+	f.WriteLine(`)`)
+	f.WriteLine(`const (`)
+	//generate constants CommandsStreamType
+	if len(constants) >= 1 {
+		for _, constant := range splitLineConstants(constants[1]) {
+			f.WriteLine(constant + `CommandsStreamType` + `=` + `"` + constant + `_commands"`)
+		}
+	}
+	f.WriteLine(`)`)
 	return f.Bytes(), nil
+}
+func splitLineConstants(constant string) []string {
+	constants := strings.Split(constant, "\n")
+	constants = constants[:len(constants)-1]
+	for i := 0; i < len(constants); i++ {
+		constants[i] = constants[i][2:]
+	}
+	return constants
 }
