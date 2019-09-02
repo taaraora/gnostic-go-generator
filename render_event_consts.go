f3c030e0f33f00a4ad23155de1f5accb4951cdf0
Olexandr Nazin <olexandr.nazin@gmail.com>
2019-09-02 15:57:26 +0300

    generating constants from components of SpecificationExtensions

--- render_event_consts.go
+++ render_event_consts.go
@@ -1,31 +1,26 @@
 package main
 
-import (
-	"strings"
-)
+import "strings"
 
 func (renderer *Renderer) Render_event_consts() ([]byte, error) {
 	f := NewLineWriter()
 	f.WriteLine(`// GENERATED FILE: DO NOT EDIT!`)
 	f.WriteLine(``)
 	f.WriteLine(`package ` + renderer.Package)
-	f.WriteLine(`// Types used by the API.`)
-	f.WriteLine(`const (`)
-	constantsFactStreamType := strings.Split(renderer.Model.FactsStreamTypes, "\n")
-	for i := 0; i < (len(constantsFactStreamType) - 1); i++ {
 
-		f.WriteLine(constantsFactStreamType[i][2:] + `FactStreamType` + `=` + `"` + constantsFactStreamType[i][2:] + `_facts"`)
+	for _, v := range renderer.Model.ComponentsSpecificationExtension {
+		if v.Value.Yaml != "" {
+
+			constants := strings.Split(v.Value.Yaml, "\n")
+			f.WriteLine(`const (`)
+			for i := 0; i < (len(constants) - 1); i++ {
+
+				f.WriteLine(constants[i][2:] + `FactStreamType` + `=` + `"` + constants[i][2:] + `_facts"`)
+
+			}
+			f.WriteLine(`)`)
+		}
 
 	}
-	f.WriteLine(`)`)
-	f.WriteLine(`const (`)
-	constantsCommandStreamType := strings.Split(renderer.Model.CommandStreamTypes, "\n")
-	for i := 0; i < (len(constantsCommandStreamType) - 1); i++ {
-
-		f.WriteLine(constantsCommandStreamType[i][2:] + `FactStreamType` + `=` + `"` + constantsCommandStreamType[i][2:] + `_facts"`)
-
-	}
-	f.WriteLine(`)`)
-
 	return f.Bytes(), nil
 }
