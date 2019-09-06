1a85359d2eb6b8dbc4a0afdfd8015c80672a6396
Olexandr Nazin <Olexandr.Nazin@gmail.com>
2019-09-06 04:18:14 +0300

    added constants to generate

--- render_event_constants_test.go
+++ render_event_constants_test.go
@@ -39,33 +39,29 @@ func Test_splitLineConstants(t *testing.T) {
 	}
 }
 
-func Test_generateStreamTypeConstants(t *testing.T) {
+func Test_generateTypeConstants(t *testing.T) {
 	testData := []struct {
-		input  string
-		result []string
-		err    error
+		inputValue        []string
+		inputTypeConstant string
+		result            []string
 	}{
 		{
-			input:  "- users_commands\n- accounts_commands\n- idp_commands\n",
-			result: []string{"const (", "UsersCommandsStreamType = \"users_commands\"", "AccountsCommandsStreamType = \"accounts_commands\"", "IdpCommandsStreamType = \"idp_commands\"", ")"},
-			err:    nil,
+			inputValue:        []string{"users_commands", "accounts_commands", "idp_commands"},
+			inputTypeConstant: "StreamType",
+			result:            []string{"const (", "UsersCommandsStreamType = \"users_commands\"", "AccountsCommandsStreamType = \"accounts_commands\"", "IdpCommandsStreamType = \"idp_commands\"", ")"},
 		},
 		{
-			input:  "- users_facts\n- accounts_facts\n- idp_facts\n",
-			result: []string{"const (", "UsersFactsStreamType = \"users_facts\"", "AccountsFactsStreamType = \"accounts_facts\"", "IdpFactsStreamType = \"idp_facts\"", ")"},
-			err:    nil,
-		},
-		{
-			input:  "properties:\n  code:\n    type: integer\n    format: int64\n    message:\n    type: string\n",
-			result: nil,
-			err:    errors.New("can't parse yaml list"),
+			inputValue:        []string{"users_facts", "accounts_facts", "idp_facts"},
+			inputTypeConstant: "StreamType",
+			result:            []string{"const (", "UsersFactsStreamType = \"users_facts\"", "AccountsFactsStreamType = \"accounts_facts\"", "IdpFactsStreamType = \"idp_facts\"", ")"},
 		},
 	}
 	for _, testCase := range testData {
-		res, err := generateStreamTypeConstants(testCase.input)
-		if err != nil && err.Error() != testCase.err.Error() {
-			t.Errorf("expected :%+q:, got :%+q:", err, testCase.err)
-		}
+		//res, err := generateTypeConstants(testCase.input, testCase.inputErr)
+		//if err != nil && err.Error() != testCase.resultErr.Error() {
+		//	t.Errorf("expected :%+q:, got :%+q:", err, testCase.resultErr)
+		//}
+		res := generateTypeConstants(testCase.inputTypeConstant, testCase.inputValue)
 		if !reflect.DeepEqual(res, testCase.result) {
 			t.Errorf("expected %+q, got %+q", res, testCase.result)
 		}
